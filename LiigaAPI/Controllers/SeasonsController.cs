54b8cb024fc0f71b79737f578e46bbef24cc9591
Iiro Puhakka <iiropuhakka@luukku.com>
2018-03-19 14:41:48 +0200

    Added matches resource implementation

--- LiigaAPI/Controllers/SeasonsController.cs
+++ LiigaAPI/Controllers/SeasonsController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Web.Http.Cors;
 using System.Net.Http;
 using System.Web.Http;
 using LiigaAPI.Models;
 
 namespace LiigaAPI.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class SeasonsController : ApiController
     {
         // GET: api/Seasons
         public HttpResponseMessage Get()
         {
+            /* Get json representation of list of seasons, create response with OK status, 
+             set content and return response.*/
             SeasonModels seasons = new SeasonModels();
 
             string json = seasons.getSeasons();
