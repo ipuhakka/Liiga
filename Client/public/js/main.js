c442319c632fd24f150351d20293cef30d1ce1a2
Iiro Puhakka <iiropuhakka@luukku.com>
2018-04-08 18:50:00 +0300

    Started UI design

--- Client/public/js/main.js
+++ Client/public/js/main.js
@@ -1,10 +1,26 @@
 //javascript source code file
 function main() {
-    getSeasons();
+    /*getSeasons();
     getTeams();
 	teams = [];
 	teams.push("Kärpät");
 	teams.push("Tappara");
 	
-	getMatches(true, 1, false, null, null, null, teams, null);
+	getMatches(true, 1, false, null, null, null, teams, null); */
+}
+
+/* click method for all switchButton-elements. Pressed button is selected. Sibling buttons are updated to notSelected.*/
+function pressedSelector(button){
+
+	button.classList.remove("switchButtonNotSelected");
+	button.classList.add("switchButtonSelected");
+	
+    siblings = button.parentNode.children;
+	
+	for (var i = 0; i < siblings.length; i++){
+		if (siblings[i] != button){
+			siblings[i].classList.remove("switchButtonSelected");
+			siblings[i].classList.add("switchButtonNotSelected");
+		}
+	}
 }
