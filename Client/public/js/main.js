0235aa060ef025c0a65470bfdc43f5aab75d8640
Iiro Puhakka <iiropuhakka@luukku.com>
2018-06-01 16:02:15 +0300

    Re-positioned client code, added alphabetical sorting for matches

--- Client/public/js/main.js
+++ Client/public/js/main.js
@@ -6,54 +6,6 @@ function main() {
 	getTeams(addSelectItemsToUI, "teamDiv", "team"); 
 }
 
-/* click method for all switchButton-elements. Pressed button is selected. Sibling buttons are updated to notSelected.*/
-function pressedSelector(button){
-
-	button.classList.remove("switchButtonNotSelected");
-	button.classList.add("switchButtonSelected");
-
-    siblings = button.parentNode.children;	
-	for (var i = 0; i < siblings.length; i++){
-		if (siblings[i] != button){			
-			siblings[i].classList.remove("switchButtonSelected");
-			siblings[i].classList.add("switchButtonNotSelected");
-		}
-	}
-}
-
-/*Selects or deselects pressed element. Pressed element contains a text element with textContent.
-
-  Gets children elements of clicked div, checks if sessionStorage contains
-  data for item_type, checks if sessionStorage contains selected item, 
-  and either deselects item or selects it. SessionStorage is updated with new array.
- */
-function itemSelected(element, item_type){
-	
-	var children = element.children;
-	
-	var items = JSON.parse(sessionStorage.getItem(item_type));
-
-	if (items == null){
-		items = [];
-	}
-	
-	if (children.length > 0){
-		text = children[0].textContent;
-		for (var i = 0; i < items.length; i++){
-			if (items[i] == text) //deselect
-			{
-				items.splice(i, 1);
-				sessionStorage.setItem(item_type, JSON.stringify(items));
-				element.style.backgroundColor = "white";
-				return;
-			}
-		}
-		items.push(text);
-		element.style.backgroundColor = "#2E4A62";
-		sessionStorage.setItem(item_type, JSON.stringify(items));
-	}
-			
-}
 
 /*
 Get search parameters, and use getMatches from api_calls.js.
@@ -120,6 +72,10 @@ function sortByParam(param, data) {
 	if (param === 'against' || param === 'lost')
 		return sortBySmallest(data, param);
 	
+	if (param === 'hometeam' || param === "awayteam"){ //fetch only the needed variables into data array
+		return sortMatchesAlphabetically(param, data);
+	}
+	
 	return sortData(data, param);
 
 }
@@ -132,14 +88,21 @@ function sortData(data, param)
     });
 }
 
+function sortMatchesAlphabetically(param, data){
+	return data.sort(function(a, b){
+    if(a[param] < b[param]) return -1;
+    if(a[param] > b[param]) return 1;
+    return 0;
+})
+}
+
 function sortBySmallest(data, param){
 	return data.sort(function (a, b) {
 		return a[param] - b[param];
     });
 }
 
-
-function alphabeticalSort(data ){
+function alphabeticalSort(data){
 	return data.sort(function(a, b){
     if(a < b) return -1;
     if(a > b) return 1;
--- Client/public/js/ui_functions.js
+++ Client/public/js/ui_functions.js
@@ -1,3 +1,52 @@
+/* click method for all switchButton-elements. Pressed button is selected. Sibling buttons are updated to notSelected.*/
+function pressedSelector(button){
+
+	button.classList.remove("switchButtonNotSelected");
+	button.classList.add("switchButtonSelected");
+
+    siblings = button.parentNode.children;	
+	for (var i = 0; i < siblings.length; i++){
+		if (siblings[i] != button){			
+			siblings[i].classList.remove("switchButtonSelected");
+			siblings[i].classList.add("switchButtonNotSelected");
+		}
+	}
+}
+
+/*Selects or deselects pressed element. Pressed element contains a text element with textContent.
+
+  Gets children elements of clicked div, checks if sessionStorage contains
+  data for item_type, checks if sessionStorage contains selected item, 
+  and either deselects item or selects it. SessionStorage is updated with new array.
+ */
+function itemSelected(element, item_type){
+	
+	var children = element.children;
+	
+	var items = JSON.parse(sessionStorage.getItem(item_type));
+
+	if (items == null){
+		items = [];
+	}
+	
+	if (children.length > 0){
+		text = children[0].textContent;
+		for (var i = 0; i < items.length; i++){
+			if (items[i] == text) //deselect
+			{
+				items.splice(i, 1);
+				sessionStorage.setItem(item_type, JSON.stringify(items));
+				element.style.backgroundColor = "white";
+				return;
+			}
+		}
+		items.push(text);
+		element.style.backgroundColor = "#2E4A62";
+		sessionStorage.setItem(item_type, JSON.stringify(items));
+	}
+			
+}
+
 //callback which handles receiving results from the server.
 function displayResults(sortedBy){
 	displayMatches(sortedBy);
