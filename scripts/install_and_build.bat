26d19679c6ada476575c520c6007b79ea9b19236
Iiro Puhakka <iiropuhakka@luukku.com>
2018-07-21 14:42:51 +0300

    Fixed api build scripts

--- scripts/install_and_build.bat
+++ scripts/install_and_build.bat
@@ -11,5 +11,5 @@ call npm run build
 cd../../scripts
 
 cd../API/build
-csc -r:Liiga.exe -r:System.Net.Http.dll -r:System.Net.Http.Formatting.dll -r:System.Web.Http.dll -r:System.Web.Http.Cors.dll -r:System.Web.Http.SelfHost.dll -r:Newtonsoft.Json.dll -out:API.exe -recurse:*.cs
+csc -r:Liiga.exe -r:System.Net.Http.dll -r:System.Net.Http.Formatting.dll -r:System.Web.Http.dll -r:System.Web.Http.Cors.dll -r:System.Web.Http.SelfHost.dll -r:Newtonsoft.Json.dll -out:API.exe -recurse:..\src\*.cs
 cd../../scripts
